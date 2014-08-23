0545781e5eca3fa6142f03859eeb0eed9edee95c
Naouak <tardiel@gmail.com>
2014-08-23 13:21:49 +0200

    First "working"demo

--- typing.js
+++ typing.js
@@ -1,45 +1,106 @@
 var Game = function(){
     var scene = document.createElement("div");
     scene.className = "game";
+    var keyLogger = new KeyLogger();
+    var lockedString = -1;
+    var last_char = 0;
 
     var characters = [
-        new Character("salut"),
-        new Character("coucou"),
-        new Character("j'ai faim"),
-        new Character("il fait pas chaud"),
-        new Character("pas faux")
     ];
+
 
     this.getScene = function(){
         return scene;
     };
 
-    this.generateCharacter = function(){
+    this.generateCharacter = function(str){
         //@todo Générer des strings en fonction du niveau
-        //@todo Ajouter les personnages dans la scene
+        if(str === undefined){
+            var strings  = ["coucou","tu veux voir ma ...","bouh","ahhhhhh !"];
+            str = strings[Math.floor(strings.length * Math.random())];
+        }
+
+
+        var char = new Character(this, str,"assets/chartest.png");
+        this.getCharacters().push(char);
+        char.addToScene(this.getScene());
     };
 
     this.getCharacters = function(){
         return characters;
     };
 
+    this.removeCharacter = function(character){
+        var i = 0;
+        var chars = this.getCharacters();
+        for(i=0; i < chars.length; i++){
+            if(chars[i] == character){
+                chars.splice(i,1);
+                break;
+            }
+        }
+    };
+
     this.think = function(){
+        var that = this;
         this.getCharacters().forEach(function(character){
-            character.think();
+            character.think(that, 1);
         });
     };
 
     this.render = function(){
-
+        var that = this;
+        this.getCharacters().forEach(function(character){
+            character.render(that);
+        });
     };
 
     this.loop = function(){
         this.think();
         this.render();
+        last_char++;
+        if(last_char == 30){
+            this.generateCharacter();
+            last_char=0;
+        }
         requestAnimationFrame(this.loop.bind(this));
     };
 
+    this.typingEvent = function(e){
+        var i = 0;
+        keyLogger.log(String.fromCharCode(e.keyCode));
+
+        if(lockedString < 0) {
+            for(i = 0; i < this.getCharacters().length; i++){
+                if(this.getCharacters()[i].checkWithLogger(keyLogger)){
+                    lockedString = i;
+                    break;
+                }
+            }
+            if(lockedString < 0){
+                keyLogger.clear();
+                return;
+            }
+        }
+
+        var strCheck = this.getCharacters()[lockedString].checkWithLogger(keyLogger);
+        if(strCheck == -1){
+            keyLogger.clear();
+            lockedString = -1;
+        } else {
+
+        }
+    };
+
+    //this.generateCharacter("salut");
+    //this.generateCharacter("coucou");
+    //this.generateCharacter("j'ai faim");
+    //this.generateCharacter("il fait pas chaud");
+    //this.generateCharacter("pas faux");
+    //this.generateCharacter("!!!!");
+
     requestAnimationFrame(this.loop.bind(this));
+    document.body.onkeypress = this.typingEvent.bind(this);
 };
 
 var KeyLogger = function(){
@@ -57,10 +118,22 @@ var KeyLogger = function(){
     };
 };
 
-var Character = function(str, sprite){
+var Character = function(game, str, sprite){
     var dist = 0;
-    var speed = 1;
+    this.speed = Math.round(Math.random()*2)+1;
     this._last_check = -1;
+    this.dead = false;
+
+    var div = document.createElement("div");
+    div.className = "character";
+    div.style.backgroundImage = "url("+sprite+")";
+    div.style.backgroundSize = "contain";
+
+    var textContainer = document.createElement("div");
+    textContainer.className = "text_container";
+    textContainer.innerText = str;
+    div.appendChild(textContainer);
+
 
     this._lock = false;
 
@@ -76,6 +149,10 @@ var Character = function(str, sprite){
         return this._lock;
     };
 
+    this.addToScene = function(scene){
+        scene.appendChild(div);
+    };
+
     this.checkWithLogger = function(logger){
         var i = 0;
         var j = 0;
@@ -86,6 +163,7 @@ var Character = function(str, sprite){
                 }
                 j++;
                 if(str.length <= j){
+                    this.die();
                     this._last_check = -1;
                     return this._last_check;
                 }
@@ -96,12 +174,19 @@ var Character = function(str, sprite){
     };
 
     this.die = function(){
-        //@todo
+        this.dead = true;
+        div.parentNode.removeChild(div);
+        game.removeCharacter(this);
     };
 
     this.think = function(game, delta){
-        dist = speed*delta;
+        if(this.dead){
+            return;
+        }
 
+        dist += this.speed*delta;
+        div.style.top = Math.round(dist)+"px";
+        div.style.zIndex = Math.round(dist);
         //GameOver conditions
         if(dist > 1000){
             this.die();
@@ -109,54 +194,9 @@ var Character = function(str, sprite){
     };
 
     this.render = function(game){
-
+        div.style.top = dist;
     };
 };
 
-
-
-var keylogger = new KeyLogger();
-
-var strings = [
-    new Character("salut"),
-    new Character("coucou"),
-    new Character("j'ai faim"),
-    new Character("il fait pas chaud"),
-    new Character("pas faux")
-];
-
-var lockedString = -1;
-
-var typing_event = function(e){
-    var i = 0;
-    keylogger.log(String.fromCharCode(e.keyCode));
-
-    if(lockedString < 0) {
-        for(i = 0; i < strings.length; i++){
-            if(strings[i].checkWithLogger(keylogger)){
-                lockedString = i;
-                break;
-            }
-        }
-        if(lockedString < 0){
-            keylogger.clear();
-            return;
-        }
-    }
-
-    var strCheck = strings[lockedString].checkWithLogger(keylogger);
-    if(strCheck == -1){
-        alert("yeah");
-        keylogger.clear();
-        lockedString = -1;
-    } else {
-
-    }
-};
-
-
 var game = new Game();
 document.body.appendChild(game.getScene());
-
-// Le temps de faire des tests
-document.onkeypress = typing_event;
